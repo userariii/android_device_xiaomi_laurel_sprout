864cdb9b82067e6259c468ee3ab192350a10541b
ARINDAM BHATTACHARJEE <abhattacharjee717@gmail.com>
2023-01-20 17:02:54 +0000

    laurel_sprout: updated vendor & clang link

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,3 +2,7 @@
 git clone https://github.com/anirudhkosgi/android-kernel-xiaomi-laurel_sprout.git kernel/xiaomi/laurel_sprout -b lineage-18.1 --depth=1
 
 # Vendor
+git clone https://gitlab.com/userariii/android_vendor_xiaomi_laurel_sprout.git vendor/xiaomi/laurel_sprout -b AOSP-13 --depth=1
+
+# Clang
+git clone https://gitlab.com/Panchajanya1999/azure-clang prebuilts/clang/host/linux-x86/clang-14.0 --depth=1
